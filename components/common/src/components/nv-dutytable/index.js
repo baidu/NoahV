25fb1f2c9115909b070f7d69f7560e05527f2428
tanruiqi <tanruiqi@baidu.com>
2019-04-12 11:26:23 +0800

    add dutyTable components

---
+++ components/common/src/components/nv-dutytable/index.js
@@ -0,0 +1,15 @@
+/**
+ * NoahV
+ * Copyright (c) 2016 Baidu, Inc. All Rights Reserved.
+ *
+ * @file NvDutyTable
+ * @author tanrich@foxmail.com
+ */
+
+import NvDutyTable from './dutyTable.vue';
+
+export {
+    NvDutyTable
+};
+
+export default NvDutyTable;
