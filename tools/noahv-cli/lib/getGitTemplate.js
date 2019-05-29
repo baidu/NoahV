71487e3f3343577f2190df8c9ef50ab39a676eab
yuyouwen <yuyouwen@baidu.com>
2019-05-29 16:06:07 +0800

    Fix template download

--- tools/noahv-cli/lib/getGitTemplate.js
+++ tools/noahv-cli/lib/getGitTemplate.js
@@ -6,90 +6,33 @@
  * @author darren(darrenywyu@gmail.com)
  */
 
-const fs = require('fs-extra')
+const fs = require('fs-extra');
 const path = require('path');
-const exists = fs.existsSync;
-const request = require('request');
-const ProgressBar = require('progress');
+const downloadGit = require('download-git-repo');
 
-const logUtil = require('../lib/logUtil');
 
-const gitRepo = 'https://api.github.com/repos/baidu/NoahV/git/trees/master';
-const rowRepo = 'https://github.com/baidu/NoahV/raw/master/';
+const templateDir = 'template';
 
-let bar;
+function filterGitFile(type, dest, cb) {
+    var fileList = fs.readdirSync(dest);
+    fileList.forEach(function (item) {
+        if (item !== templateDir) {
+            fs.removeSync(path.resolve(dest, item));
+        }
+    });
+    fs.moveSync(path.resolve(dest, templateDir, type), dest);
+    fs.removeSync(path.resolve(dest, templateDir));
+    cb();
+}
 
-/**
- * downLoad file 
- * @param  {Object} file  file in github
- * @param  {string} type  folder
- * @param  {string} dest  file path 
- * @return {boolean}  download status
- */
-function downLoad(file, type, dest, cb) {
-    const filePath = dest + file.path.replace(type, '');
-    const folder = path.dirname(filePath);
-    if (!fs.existsSync(folder)) {
-        fs.mkdirpSync(folder, { recursive: true }, err => {
-            if (err) {
-                logUtil.error('noahv-cli', 'error', err);
-                return false;
-            }
-        });
-    }
-
-    request(rowRepo + '/' + file.path, (err, res, body) => {
+// getTemplate form github
+function getTemplate(type, dest, cb) {
+    downloadGit('github:baidu/NoahV', dest, function(err) {
         if (err) {
-            logUtil.error('noahv-cli', 'error', err);
-            return false;
+            cb('Get template fail!');
         }
         else {
-            bar.tick();
-            if (bar.complete) {
-                cb();
-            }
-        }
-    }).pipe(fs.createWriteStream(filePath));
-}
-
-/**
- * filter && download file from github
- * @param  {Array}   tree  file in NoahV repo
- * @param  {string}  type  folder
- * @param  {string}  dest  file path 
- * @param  {Function} cb   callback
- */
-function filterTree(tree, type, dest, cb) {
-    tree = tree.filter(function (item) {
-        return item.type === 'blob' && item.path.indexOf(type) > -1;
-    });
-    bar = new ProgressBar('downloading [:bar] :rate/bps :percent :etas', {
-        complete: '=',
-        incomplete: ' ',
-        width: 20,
-        total: tree.length
-    });
-    for (let i = 0; i < tree.length; i++) {
-        let isDownLoad = downLoad(tree[i], type, dest, cb);
-        if (isDownLoad === false) {
-            cb('Get template fail!');
-            break;
-        }
-    }
-}
-
-// getTemplate form github floder 
-function getTemplate(type, dest, cb) {
-    request({
-        url: gitRepo + '?recursive=1',
-        headers: {
-            'User-Agent': 'noahv'
-        }
-    }, (err, res, body) => {
-        if (!err && res.statusCode === 200) {
-            const tree = JSON.parse(body)['tree'];
-            type = 'template/' + type;
-            filterTree(tree, type, dest, cb);
+            filterGitFile(type, dest, cb);
         }
     });
 }
