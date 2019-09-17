47d9387eb7a94b842ba9e4174e7f669848f5f03c
yuyouwen <yuyouwen@baidu.com>
2019-09-17 17:47:35 +0800

    Remove theme demo

--- template/common/src/common/header.js
+++ template/common/src/common/header.js
@@ -43,75 +43,8 @@ export default {
             ]
         },
         {
-            label: '主题示例',
-            key: 'themes',
-            children: [
-                {
-                    label: '按钮示例',
-                    key: 'theme/button',
-                    link: 'theme/button'
-                },
-                {
-                    label: '表格示例',
-                    key: 'theme/table',
-                    link: 'theme/table'
-                },
-                {
-                    label: '输入框示例',
-                    key: 'input',
-                    link: 'theme/input'
-                },
-                {
-                    label: '单选按钮示例',
-                    key: 'radio',
-                    link: 'theme/radio'
-                },
-                {
-                    label: '复选按钮示例',
-                    key: 'checkbox',
-                    link: 'theme/checkbox'
-                },
-                {
-                    label: '浮窗示例',
-                    key: 'toast',
-                    link: 'theme/toast'
-                },
-                {
-                    label: '提示说明示例',
-                    key: 'tooltip',
-                    link: 'theme/tooltip'
-                },
-                {
-                    label: '弹窗示例',
-                    key: 'modal',
-                    link: 'theme/modal'
-                },
-                {
-                    label: '标签示例',
-                    key: 'tabs',
-                    link: 'theme/tabs'
-                },
-                {
-                    label: '分页示例',
-                    key: 'page',
-                    link: 'theme/page'
-                },
-                {
-                    label: '下拉菜单示例',
-                    key: 'select',
-                    link: 'theme/select'
-                },
-                {
-                    label: '开关示例',
-                    key: 'switch',
-                    link: 'theme/switch'
-                // },
-                // {
-                //     label: '搜索框示例',
-                //     key: 'find',
-                //     link: 'theme/find'
-                }
-            ]
+            label: '使用文档',
+            linkTarget: 'https://www.noahv.org/#/doc/component/icon'
         }
     ],
     hasBread: true,
--- template/common/src/common/router.js
+++ template/common/src/common/router.js
@@ -26,58 +26,6 @@ export default [
         component: r => require.ensure([], () => r(require('src/demo/chart/chartDemo')), 'chartDemo'),
         path: '/chartDemo'
     },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/button')), 'button'),
-        path: '/theme/button'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/table')), 'table'),
-        path: '/theme/table'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/input')), 'input'),
-        path: '/theme/input'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/checkbox')), 'checkbox'),
-        path: '/theme/checkbox'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/radio')), 'radio'),
-        path: '/theme/radio'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/toast')), 'toast'),
-        path: '/theme/toast'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/tooltip')), 'tooltip'),
-        path: '/theme/tooltip'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/modal')), 'modal'),
-        path: '/theme/modal'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/tabs')), 'tabs'),
-        path: '/theme/tabs'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/page')), 'page'),
-        path: '/theme/page'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/select')), 'select'),
-        path: '/theme/select'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/switch')), 'switch'),
-        path: '/theme/switch'
-    },
-    {
-        component: r => require.ensure([], () => r(require('src/demo/theme/find')), 'find'),
-        path: '/theme/find'
-    },
     // please keep this object last one
     {
         component: r => require.ensure([], () => r(require('src/common/layout/404')), '404'),
