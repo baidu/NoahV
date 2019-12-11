260f2c5d604d50da956c694893cb91165a11c691
cuiyuan <cuiyuan@baidu.com>
2019-12-11 15:32:52 +0800

    Add tooltip for NvIcon

--- components/common/src/components/nv-icon/icon.vue
+++ components/common/src/components/nv-icon/icon.vue
@@ -1,5 +1,15 @@
 <template>
-  <i :class="classes"></i>
+    <Tooltip :content="content"
+             :placement="placement"
+             :disabled="!hasTooltip"
+             :delay="delay"
+             :always="always"
+             :theme="theme"
+             :max-width="maxWidth"
+             :offset="offset"
+             :transfer="transfer">
+        <i :class="classes" :style="{cursor: cursor}"></i>
+    </Tooltip>
 </template>
 
 <script>
@@ -8,7 +18,48 @@ import getClassName from '../utils.js';
 export default {
     name: 'NvIcon',
     props: {
-        type: String
+        type: String,
+        content: {
+            type: String,
+            default: ''
+        },
+        placement: {
+            type: String,
+            default: 'bottom'
+        },
+        delay: {
+            type: Number,
+            default: 0
+        },
+        always: {
+            type: Boolean,
+            default: false
+        },
+        theme: {
+            type: String,
+            default: 'light'
+        },
+        maxWidth: {
+            type: [String, Number],
+            default: 100
+        },
+        offset: {
+            type: Number,
+            default: 0
+        },
+        transfer: {
+            type: Boolean,
+            default: false
+        },
+        cursor: {
+            type: String,
+            default: 'default'
+        }
+    },
+    data() {
+        return {
+            hasTooltip: this.content.length !== 0
+        };
     },
     computed: {
         classes() {
