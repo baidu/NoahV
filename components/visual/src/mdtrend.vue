9112b897d978e4bf9024581f464fe1b59dcd25c7
gxfmqfzkzdgzwj <597785713@qq.com>
2020-02-04 10:15:24 +0800

    fix(component): fix mdtrend toolbox and style (#99)
    * fix mdtrend toolbox and style
    
    * fix(components): fix options not merge bug in nvtrend
    
    * fix(components): fix toolbox in nvMdtrend
    
    Co-authored-by: DarrenY <darrenywyu@gmail.com>

--- components/visual/src/mdtrend.vue
+++ components/visual/src/mdtrend.vue
@@ -1074,7 +1074,7 @@ export default {
                                 line: 'image://' + chartlinePng,
                                 bar: 'image://' + chartcolumnPng,
                                 stack: 'image://' + chartstackPng,
-                                tiled: 'image://' + chartnormalPng
+                                // tiled: 'image://' + chartnormalPng
                             },
                             option: {
                                 line: {
